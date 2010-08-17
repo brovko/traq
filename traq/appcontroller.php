043e3364d24194fb086e7b36025df51d2bfebc93
Jack Polgar <jack.polgar@gmail.com>
2010-08-18 06:45:10 +1000

    AppController for Traq.

---
+++ traq/appcontroller.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Traq
+ * Copyright (C) 2009, 2010 Jack Polgar
+ *
+ * This file is part of Traq.
+ * 
+ * Traq is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3 only.
+ * 
+ * Traq is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Traq. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+class AppController extends Controller
+{
+	public $project = null;
+	
+	public function __construct()
+	{
+		parent::__construct();
+		
+		if(isset($this->uri->seg[1]) && $this->is_project($this->uri->seg[1]))
+		{
+			$this->project = $this->db->query_first("SELECT * FROM traq_projects WHERE slug='".$this->db->es($this->uri->seg[1])."' LIMIT 1");
+		}
+		
+		$this->load->helper('html');
+	}
+	
+	public function is_project($slug)
+	{
+		if($this->db->num_rows($this->db->query("SELECT * FROM traq_projects WHERE slug='".$this->db->es($slug)."' LIMIT 1")))
+			return true;
+		
+		return false;
+	}
+}
